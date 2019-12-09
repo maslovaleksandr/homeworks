f8a65f21fe6fa6df5255852623e2fb8850ac6133
Aleksandr Maslov <a.mslv@outlook.com>
2019-12-09 15:33:05 +0300

    hot fix

--- homework7/time_decorator.py
+++ homework7/time_decorator.py
@@ -5,8 +5,7 @@ def time_result(func):
     def inner():
         start_time = time.time()
         func()
-        return print(time.time() - start_time)
-        # return print('job was done in {}'.format(time.strftime("%S",(end_time - start_time)))
+        return print("Function run time is: {}".format(time.time() - start_time))
     return inner
 
 
