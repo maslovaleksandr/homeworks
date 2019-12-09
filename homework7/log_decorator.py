4f9e7e81d3e5a2d1100bfb7f18b5929a19fc684e
Aleksandr Maslov <a.mslv@outlook.com>
2019-12-09 15:44:49 +0300

    log start/end function

---
+++ homework7/log_decorator.py
@@ -0,0 +1,25 @@
+import time
+
+
+def add_log(func):
+    def inner():
+        print("{} started at: {}".format(func.__name__, time.strftime("%H:%M:%S", time.gmtime())))
+        func()
+        print("{} ended at: {}".format(func.__name__, time.strftime("%H:%M:%S", time.gmtime())))
+    return inner
+
+
+@add_log
+def some_func():
+    print('hello wolrd')
+
+
+@add_log
+def print_list():
+    new_list = [i for i in range(1, 1000)]
+    print(new_list)
+
+
+some_func()
+print_list()
+
