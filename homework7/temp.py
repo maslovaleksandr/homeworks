e16fb9f4e454c8bc5100a9ef7f9480f4f300262c
Aleksandr Maslov <a.mslv@outlook.com>
2019-12-11 11:43:31 +0300

    hot fix

---
+++ homework7/temp.py
@@ -0,0 +1,14 @@
+class player:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self):
+        pass
+
+
+class ships:
+    def __init__(self, ship_count, ship_health):
+        self.ship_count = ship_count
+        self.ship_health = ship_health
+
+    def place_ship(self):
