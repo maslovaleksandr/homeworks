853cf0e79aad7726d6494d5b9c1ee79e1510fa5a
Aleksandr Maslov <a.mslv@outlook.com>
2019-12-12 22:48:29 +0300

    flask tests

--- requests/application.py
+++ requests/application.py
@@ -29,9 +29,25 @@ def print_subpath(subpath):
     return "SubPath is: " + subpath
 
 
-@application.route("/pow/<int:x>")
-def cal_pow(x):
-    return "Pow of " + str(x) + " is " + str(x**2)
+@application.route("/long/<value1>/<value2>/<value3>")
+def find_longest(value1, value2, value3):
+    max_value = max(len(value1), len(value2), len(value3))
+    return str(max_value)
+
+
+@application.route("/sum/<int:x>/<int:y>")
+def cal_pow(x, y):
+    return "Sum x,y is: " + str(x+y)
+
+
+@application.route("/check_file/<path:value>")
+def look_for_file(value):
+    with open(value, mode='r') as f:
+        if f:
+            return "File exists"
+        else:
+            return "File no exists"
+
 
 
 @application.route("/htmlpage")
